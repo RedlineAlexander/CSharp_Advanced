129c4ef2255b909c5317ffdf9d26026d772787eb
OlexandrKukhar <45728829+RedlineAlexander@users.noreply.github.com>
2020-04-24 20:10:38 +0300

    qwe

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
